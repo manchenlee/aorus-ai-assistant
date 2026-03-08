804132272ae615445267bca542865af413afeb0e
manchenlee <sleep1700505@gmail.com>
2026-03-09 02:04:52 +0800

    fix bugs

--- src/utils.py
+++ src/utils.py
@@ -6,9 +6,7 @@ def validate_query(user_query):
         if ord(char) <= 127:
             score += 1 
         else:
-            score += 3
-            
-    Config.MAX_CHARS = 300 
+            score += 3 
     
     if score > Config.MAX_CHARS:
         return False, f"[Error]: 輸入過長，請精簡問題後再試一次。 Current input query is too long. Please provide a more concise version and try again. (Current character score: {score}/{Config.MAX_CHARS})"
